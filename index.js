03afdafa3e18ea40de3b0e90ec5612fe6e5c7775
ChloeShepard <79712700+ChloeShepard@users.noreply.github.com>
2022-07-13 23:36:11 +0200

    Run index.js with a node process.

---
+++ index.js
@@ -0,0 +1,207 @@
+const fs = require('node:fs');
+const { Client, Collection, Intents, MessageActionRow, MessageButton } = require('discord.js');
+const { token, clientId, guildId } = require('./config.json');
+const { sheHerId, sheTheyId, heHimId, heTheyId, theyThemId, theySheId, theyHeId, anyPronounsId } = require('./variables.json');
+
+// const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+// Import relevant classes from discord.js
+//const { Client, Intents, Collection, MessageActionRow, MessageButton } = require('discord.js');
+
+// Instantiate a new client with some necessary parameters.
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+//console.log(client);
+//console.log("\n\n" + client.users);
+
+//const michi = client.users.fetch('950699356313505802');
+//michi.setUsername('Michi Supremo');
+
+client.commands = new Collection();
+// const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+	// commands.push(command.data.toJSON());
+}
+
+/*
+
+for (const file of eventFiles) {
+	const event = require('./events/${file}');
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+*/
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	// console.log(client.commands)
+	// console.log(interaction)
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		return command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isButton()) return;
+	
+	const userRoles = interaction.member.roles.cache;
+	
+	switch (interaction.customId) {
+		
+		case 'She / Her': 
+		if (userRoles.get(sheHerId)) {
+			
+			interaction.member.roles.remove(sheHerId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(sheHerId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		
+		
+		case 'She / They':
+		if (userRoles.get(sheTheyId)) {
+			
+			interaction.member.roles.remove(sheTheyId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(sheTheyId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		case 'He / Him': 
+		if (userRoles.get(heHimId)) {
+			
+			interaction.member.roles.remove(heHimId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(heHimId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		case 'He / They': 
+		if (userRoles.get(heTheyId)) {
+			
+			interaction.member.roles.remove(heTheyId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(heTheyId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		case 'They / Them': 
+		if (userRoles.get(theyThemId)) {
+			
+			interaction.member.roles.remove(theyThemId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(theyThemId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		case 'They / She': 
+		if (userRoles.get(theySheId)) {
+			
+			interaction.member.roles.remove(theySheId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(theySheId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+
+		case 'They / He': 
+		if (userRoles.get(theyHeId)) {
+			
+			interaction.member.roles.remove(theyHeId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(theyHeId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		case 'Any Pronouns': 
+		if (userRoles.get(anyPronounsId)) {
+			
+			interaction.member.roles.remove(anyPronounsId); 
+			await interaction.reply({ content: `Has deseleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		}
+		else { 
+			
+			interaction.member.roles.add(anyPronounsId); 
+			await interaction.reply({ content: `Has seleccionado los pronombres ${interaction.customId}`, ephemeral: true});
+			
+		} break;
+		
+		
+	}
+	
+});
+
+/*
+client.on('interactionCreate', interaction => {
+	(async() => {
+		console.log('before start');
+		
+		if (!interaction.isCommand()) return;
+		console.log(commands[0].name);
+		
+		if (interaction.commandName === 'ping') {
+			await interaction.reply('Pong!');
+		}
+		  
+		console.log('after start');
+	})();
+});;
+*/
+
+client.login(token);
---
+++ michi.js
@@ -0,0 +1,459 @@
+const fs = require('fs')
+var FrasesDM1 = new Array();
+
+module.exports = {
+	randomInt: function Michi(min,max,dm,numDados) {
+		return getRandomInt(min,max,dm,numDados)
+	}
+};
+
+function getRandomInt(min, max, dm, numDados) {
+		
+	var resul = '';
+	var total = 0;
+	var maxTotal = (max-1)*numDados;
+	msg = '';
+	
+	if (numDados == 1) {
+		
+		resul = Math.floor(Math.random() * (max - min)) + min;
+		
+		max = max-1;
+		
+		return MensajeDado(resul, max, dm, numDados);
+		
+	}
+	
+	else {
+		
+		for (var i=1; i<=numDados; i++) {
+		
+			resul = Math.floor(Math.random() * (max - min)) + min;
+			msg += "Dado Nº " + i + ": " + resul + "\n";
+			total += resul;
+			
+		}
+		
+		msg += "\nTotal: " + total;
+		msg += "\n\n" + MensajeDado(total, maxTotal, dm, numDados);
+		
+	}
+	
+	console.log(FrasesDM1)
+	return msg;
+	
+}
+
+function MensajeDado(num, dado, dm, numDados) {
+	
+	// var username = member.user.username
+	var min = dado/dado;
+	var tercioInferior = Math.floor(dado / 3);
+	var tercioSuperior = Math.floor(tercioInferior * 2);
+	var msg = '';
+	
+	if (dm == true && numDados == 1) {
+		
+		if (num == min) {
+		
+			msg = fraseRandom(num, dado, 'Frases1dDM1', numDados);
+		
+		}
+	
+		else if (num < tercioInferior && num > min) {
+			
+			msg = fraseRandom(num, dado, 'Frases1dDM2', numDados);
+			
+		}
+		
+		else if (num >= tercioInferior && num <= tercioSuperior) {
+			
+			msg = fraseRandom(num, dado, 'Frases1dDM3', numDados);
+			
+		}
+		
+		else if (num > tercioSuperior && num < dado){
+			
+			msg = fraseRandom(num, dado, 'Frases1dDM4', numDados);
+			
+		}
+		
+		else {
+			
+			msg = fraseRandom(num, dado, 'Frases1dDM5', numDados);
+			
+		}
+		
+	}
+	
+	else if (dm == false && numDados == 1) {
+		
+		if (num == min) {
+		
+			msg = fraseRandom(num, dado, 'Frases1dPL1', numDados);
+		
+		}
+	
+		else if (num < tercioInferior && num > min) {
+			
+			msg = fraseRandom(num, dado, 'Frases1dPL2', numDados);
+			
+		}
+		
+		else if (num >= tercioInferior && num <= tercioSuperior) {
+			
+			msg = fraseRandom(num, dado, 'Frases1dPL3', numDados);
+			
+		}
+		
+		else if (num > tercioSuperior && num < dado){
+			
+			msg = fraseRandom(num, dado, 'Frases1dPL4', numDados);
+			
+		}
+		
+		else {
+			
+			msg = fraseRandom(num, dado, 'Frases1dPL5', numDados);
+			
+		}
+		
+	}
+	
+	else if (dm == true && numDados != 1) {
+		
+		if (num == min) {
+			
+			msg = fraseRandom(num, dado, 'FrasesDM1', numDados);
+		
+		}
+	
+		else if (num < tercioInferior && num > min) {
+			
+			msg = fraseRandom(num, dado, 'FrasesDM2', numDados);
+			
+		}
+		
+		else if (num >= tercioInferior && num <= tercioSuperior) {
+			
+			msg = fraseRandom(num, dado, 'FrasesDM3', numDados);
+			
+		}
+		
+		else if (num > tercioSuperior && num < dado){
+			
+			msg = fraseRandom(num, dado, 'FrasesDM4', numDados);
+			
+		}
+		
+		else {
+			
+			msg = fraseRandom(num, dado, 'FrasesDM5', numDados);
+			
+		}
+			
+	}
+	
+	else if (dm == false && numDados != 1) {
+	
+		if (num == min) {
+		
+			msg = fraseRandom(num, dado, 'FrasesPL1', numDados);
+		
+		}
+	
+		else if (num < tercioInferior && num > min) {
+			
+			msg = fraseRandom(num, dado, 'FrasesPL2', numDados);
+			
+		}
+		
+		else if (num >= tercioInferior && num <= tercioSuperior) {
+			
+			msg = fraseRandom(num, dado, 'FrasesPL3', numDados);
+			
+		}
+		
+		else if (num > tercioSuperior && num < dado){
+			
+			msg = fraseRandom(num, dado, 'FrasesPL4', numDados);
+			
+		}
+		
+		else {
+			
+			msg = fraseRandom(num, dado, 'FrasesPL5', numDados);
+			
+		}
+		
+	}
+	
+	// var msg = "X> " + num + " + " + min + " + " + dado + " + " + tercioInferior + " + " + tercioSuperior;
+	// var msg = roles;
+	
+	return msg;
+	//console.log(msg)
+		
+}
+
+function fraseRandom(num, dado, array, numDados, callback) {
+	
+	if (array == 'Frases1dPL1') {
+		
+		var FrasesPL1 = new Array();
+		FrasesPL1.push('Oh oh un ' + num + ', RIP :skull_crossbones:');
+		FrasesPL1.push('Uffff un ' + num + ', RIP :skull_crossbones:');
+		var random = Math.floor(Math.random() * FrasesPL1.length);
+		
+		return FrasesPL1[random];
+		
+	}
+	
+	if (array == 'Frases1dPL2') {
+		
+		var FrasesPL2 = new Array();
+		FrasesPL2.push('Aishhhh un ' + num + ', no tienes mucha suerte :crying_cat_face:');
+		var random = Math.floor(Math.random() * FrasesPL2.length);
+		
+		return FrasesPL2[random];
+		
+	}
+	
+	if (array == 'Frases1dPL3') {
+		
+		var FrasesPL3 = new Array();
+		FrasesPL3.push('Un ' + num + ', no está del todo mal :smiley_cat:');
+		var random = Math.floor(Math.random() * FrasesPL3.length);
+		
+		return FrasesPL3[random];
+		
+	}
+	
+	if (array == 'Frases1dPL4') {
+		
+		var FrasesPL4 = new Array();
+		FrasesPL4.push('Un ' + num + ' que buena tirada! :heart_eyes_cat:');
+		var random = Math.floor(Math.random() * FrasesPL4.length);
+		
+		return FrasesPL4[random];
+		
+	}
+	
+	if (array == 'Frases1dPL5') {
+		
+		var FrasesPL5 = new Array();
+		FrasesPL5.push('Super ' + num + ' vaya suerte!!! :scream_cat:');
+		FrasesPL5.push('Super ' + num + ' vaya suerte!!! :scream_cat:');
+		FrasesPL5.push('Super ' + num + ' vaya suerte!!! :scream_cat:');
+		FrasesPL5.push('Super ' + num + ' vaya suerte!!! :scream_cat:');
+		var random = Math.floor(Math.random() * FrasesPL5.length);
+		
+		return FrasesPL5[random];
+		
+	}
+	
+	if (array == 'Frases1dDM1') {
+
+		if (fs.existsSync('frasesDM1.json')) {
+
+			fs.readFileSync('frasesDM1.json', function (err, data) {
+
+				var json = JSON.parse(data)
+
+				Object.keys(json).forEach(function(key){
+							
+					FrasesDM1.push(json[key])
+
+				});
+
+				//Funciona
+				callback()
+
+			})
+
+			// No funciona!?!??!?!?!?!?!!??
+			console.log(FrasesDM1)
+
+		}
+
+		else {
+
+			FrasesDM1.push("Agh... 1... Creo que las estrellas se han alineado para vosotros...");
+			FrasesDM1.push("¿1? En fin... Supongo que hay gente con menos suerte...");
+			FrasesDM1.push("Esto no debería de ser asi... Un maldito 1.");
+			FrasesDM1.push("¿¡1!?, ¡La ultima vez que pierdo! No os acostumbréis.");
+			
+		}
+
+		var random = Math.floor(Math.random() * FrasesDM1.length)
+		//console.log(FrasesDM1[random])
+		return FrasesDM1[random]
+
+	}
+	
+	if (array == 'Frases1dDM2') {
+		
+		var FrasesDM2 = new Array();
+		FrasesDM2.push("Vaya, un " + num + "... Esta vez os salvareis...");
+		FrasesDM2.push("MALDITA SEA UN " + num + ". Con un poco mas de suerte hasta te doy una piruleta legendaria");
+		FrasesDM2.push("Otra vez, ahora un " + num + "... Quien sabe que hubiera sido un 1...");
+		FrasesDM2.push("Poca suerte con ese " + num + "... La próxima vez será");
+		var random = Math.floor(Math.random() * FrasesDM2.length);
+		
+		return FrasesDM2[random];
+		
+	}
+	
+	if (array == 'Frases1dDM3') {
+		
+		var FrasesDM3 = new Array();
+		FrasesDM3.push("Un " + num + " eh... ¿Bastante decente por esta vez no?");
+		FrasesDM3.push("Hey un " + num + "... La cosa se pone interesante...");
+		FrasesDM3.push("Bueno un " + num +". No esta mal para empezar...");
+		FrasesDM3.push("Tampoco un " + num + " es lo mejor del mundo, pero es algo...");
+		var random = Math.floor(Math.random() * FrasesDM3.length);
+		
+		return FrasesDM3[random];
+		
+	}
+	
+	if (array == 'Frases1dDM4') {
+		
+		var FrasesDM4 = new Array();
+		FrasesDM4.push("Hehehe... ese " + num + " pudiera haber sido un " + dado + "...");
+		FrasesDM4.push("¡Un " + num + "! La proxima vez... Será aun mas.");
+		FrasesDM4.push("¡HA! Un " + num + ", ¡Un día llegaré a ese " + dado + "!");
+		FrasesDM4.push("Este " + num + " dolerá.");
+		var random = Math.floor(Math.random() * FrasesDM4.length);
+		
+		return FrasesDM4[random];
+		
+	}
+	
+	if (array == 'Frases1dDM5') {
+		
+		var FrasesDM5 = new Array();
+		FrasesDM5.push(num + ". HAHAAHAHAHAHAHAHH.");
+		FrasesDM5.push("CUANDO LLEGA UN " + num + ", A VECES TOCA DIVERTIRSE.");
+		FrasesDM5.push("¡HAHAHAHAHA UN " + num + "!, AQUÍ LLEGA EL COCO.");
+		FrasesDM5.push("EL INFIERNO NO SERÁ NADA COMPARADO CONMIGO, DADLE GRACIAS A ESE " + num + ".");
+		var random = Math.floor(Math.random() * FrasesDM5.length);
+		
+		return FrasesDM5[random];
+		
+	}
+	
+	if (array == 'FrasesDM1') {
+		
+		var FrasesDM1 = new Array();
+		FrasesDM1.push("Agh... Creo que las estrellas se han alineado para vosotros...");
+		FrasesDM1.push("En fin... Supongo que hay gente con menos suerte...");
+		FrasesDM1.push("Esto no debería de ser asi...");
+		FrasesDM1.push("¡La ultima vez que pierdo! No os acostumbréis.");
+		var random = Math.floor(Math.random() * FrasesDM1.length);
+		
+		return FrasesDM1[random];
+		
+	}
+	
+	if (array == 'FrasesDM2') {
+		
+		var FrasesDM2 = new Array();
+		FrasesDM2.push("Vaya, Esta vez os salvareis...");
+		FrasesDM2.push("Con un poco mas de suerte hasta te doy una piruleta legendaria.");
+		FrasesDM2.push("Poca suerte eh... La próxima vez será.");
+		var random = Math.floor(Math.random() * FrasesDM2.length);
+		
+		return FrasesDM2[random];
+		
+	}
+	
+	if (array == 'FrasesDM3') {
+		
+		var FrasesDM3 = new Array();
+		FrasesDM3.push("¿Bastante decente por esta vez no?");
+		FrasesDM3.push("La cosa se pone interesante...");
+		FrasesDM3.push("No esta mal para empezar...");
+		FrasesDM3.push("Tampoco es lo mejor del mundo, pero es algo...");
+		var random = Math.floor(Math.random() * FrasesDM3.length);
+		
+		return FrasesDM3[random];
+		
+	}
+	
+	if (array == 'FrasesDM4') {
+		
+		var FrasesDM4 = new Array();
+		FrasesDM4.push("La proxima vez... Será aun mas.");
+		FrasesDM4.push("Esto dolerá.");
+		var random = Math.floor(Math.random() * FrasesDM4.length);
+		
+		return FrasesDM4[random];
+		
+	}
+	
+	if (array == 'FrasesDM5') {
+		
+		var FrasesDM5 = new Array();
+		FrasesDM5.push("HAHAAHAHAHAHAHAHH");
+		FrasesDM5.push("A VECES TOCA DIVERTIRSE.");
+		FrasesDM5.push("AQUÍ LLEGA EL COCO.");
+		FrasesDM5.push("EL INFIERNO NO SERÁ NADA COMPARADO CONMIGO.");
+		var random = Math.floor(Math.random() * FrasesDM5.length);
+		
+		return FrasesDM5[random];
+		
+	}	
+	
+	if (array == 'FrasesPL1') {
+		
+		var FrasesPL1 = new Array();
+		FrasesPL1.push('Oh oh, RIP :skull_crossbones:');
+		FrasesPL1.push('Uffff, RIP :skull_crossbones:');
+		var random = Math.floor(Math.random() * FrasesPL1.length);
+		
+		return FrasesPL1[random];
+		
+	}
+	
+	if (array == 'FrasesPL2') {
+		
+		var FrasesPL2 = new Array();
+		FrasesPL2.push('Aishhhh, no tienes mucha suerte :crying_cat_face:');
+		var random = Math.floor(Math.random() * FrasesPL2.length);
+		
+		return FrasesPL2[random];
+		
+	}
+	
+	if (array == 'FrasesPL3') {
+		
+		var FrasesPL3 = new Array();
+		FrasesPL3.push('No está del todo mal :smiley_cat:');
+		var random = Math.floor(Math.random() * FrasesPL3.length);
+		
+		return FrasesPL3[random];
+		
+	}
+	
+	if (array == 'FrasesPL4') {
+		
+		var FrasesPL4 = new Array();
+		FrasesPL4.push('Que buenas tiradas! :heart_eyes_cat:');
+		var random = Math.floor(Math.random() * FrasesPL4.length);
+		
+		return FrasesPL4[random];
+		
+	}
+	
+	if (array == 'FrasesPL5') {
+		
+		var FrasesPL5 = new Array();
+		FrasesPL5.push('Super ' + dado + ' vaya suerte!!! :scream_cat:');
+		var random = Math.floor(Math.random() * FrasesPL5.length);
+		
+		return FrasesPL5[random];
+		
+	}
+	
+}
